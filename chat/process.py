40679ba045d8ab0d916f273387b3f5519ddd6b72
Ifiok Ambrose <ifiokambrose@gmail.com>
2023-12-15 14:49:02 +0000

    Added Logging to process.py file

--- chat/process.py
+++ chat/process.py
@@ -1,15 +1,15 @@
-# your_app/management/commands/process_video.py
-
 import cv2
 import asyncio
 import websockets
+import logging  # Import the logging module
 from django.core.management.base import BaseCommand
+
+logger = logging.getLogger(__name__)  # Create a logger instance
 
 class Command(BaseCommand):
     help = 'Process video stream and broadcast to WebSocket'
 
     async def video_processing(self, websocket):
-        # Open the video stream (replace 'your_mpeg_url' with the actual MPEG URL)
         cap = cv2.VideoCapture('196.168.x.x')
 
         while True:
@@ -18,19 +18,23 @@ class Command(BaseCommand):
             if not ret:
                 break
 
-            # Convert the frame to grayscale
             gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            # Send the grayscale frame to the WebSocket
             await websocket.send(gray_frame.tobytes())
 
-            # Adjust the delay based on your video frame rate
             await asyncio.sleep(0.1)
 
         cap.release()
 
     def handle(self, *args, **options):
-        asyncio.get_event_loop().run_until_complete(
-            websockets.serve(self.video_processing, 'ws://127.0.0.1:8000/ws/video/processed/', 8765)
-        )
+        logger.info("Starting video processing")  # Log a message when the command starts
+
+        try:
+            asyncio.get_event_loop().run_until_complete(
+                websockets.serve(self.video_processing, 'ws://127.0.0.1:8000/ws/video/processed/', 8765)
+            )
+        except Exception as e:
+            logger.error(f"An error occurred: {e}")  # Log an error if an exception occurs
+
+        logger.info("Video processing completed")  # Log a message when the command completes
         asyncio.get_event_loop().run_forever()
