a10f87eed940fcffc2f3510087390683cd3a614f
Ifiok Ambrose <ifiokambrose@gmail.com>
2023-12-15 14:55:16 +0000

    E don cast

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,5 +1,4 @@
-# your_app/consumers.py
-
+# consumers.py
 import cv2
 import asyncio
 import websockets
@@ -13,11 +12,11 @@ class VideoConsumer(AsyncWebsocketConsumer):
         pass
 
     async def receive(self, text_data):
+        # You may process the received text_data if needed
         pass
 
-    async def video_processing(self, websocket):
-        # Open the video stream (replace 'your_mpeg_url' with the actual MPEG URL)
-        cap = cv2.VideoCapture('http://192.168.0.2:8080/')
+    async def video_processing(self):
+        cap = cv2.VideoCapture('http://192.168.0.2:8080/video')
 
         while True:
             ret, frame = cap.read()
@@ -25,19 +24,11 @@ class VideoConsumer(AsyncWebsocketConsumer):
             if not ret:
                 break
 
-            # Convert the frame to grayscale
             gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
             # Send the grayscale frame to the WebSocket
-            await websocket.send(gray_frame.tobytes())
+            await self.send(text_data=gray_frame.tobytes())
 
-            # Adjust the delay based on your video frame rate
             await asyncio.sleep(0.1)
 
         cap.release()
-
-    async def receive(self, text_data):
-        websocket_url = 'ws://127.0.0.1:8000/ws/video/processed/'
-        
-        # Start the video processing within the consumer
-        await self.video_processing(self.channel_layer.get('websocket.receive'))
